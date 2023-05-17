1f1941452ec83f0743d03659791bebe969c39dc5
dolatjangir <121333475+dolatjangir@users.noreply.github.com>
2023-05-17 18:20:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+  
+import './App.css';
+
+import Board from "./tictactoe/Board"
+
+
+function App() {
+ 
+  return (
+    <div className="App"> 
+    <Board/>
+    </div>
+    );
+}
+
+export default App;
